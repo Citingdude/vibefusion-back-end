b89b199d38811b35fed0ec4a3a302e78974556fc
citingdude <glenn.reumers@hotmail.com>
2022-03-04 20:34:19 +0100

    Create BlogController

---
+++ app/Controllers/BlogController.ts
@@ -0,0 +1,24 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class BlogController {
+  public async index(ctx: HttpContextContract) {
+    return [
+      {
+        id: 1,
+        title: 'Hello blogs',
+      },
+      {
+        id: 2,
+        title: 'Hello blogs 2',
+      }
+    ]
+  },
+
+  public async view(ctx: HttpContextContract) {
+    return [
+      {
+        slug: ctx.params
+      }
+    ]
+  }
+}
