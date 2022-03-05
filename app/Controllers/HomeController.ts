e166eaf5a31cd773a910f1077e874fe6038272c3
citingdude <glenn.reumers@hotmail.com>
2022-03-05 17:23:51 +0100

    Add index + create function for HomeController

--- app/Controllers/HomeController.ts
+++ app/Controllers/HomeController.ts
@@ -1,16 +1,26 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Page from 'App/Models/Page'
 
 export default class HomeController {
   public async index(ctx: HttpContextContract) {
-    return [
-      {
-        id: 1,
-        title: 'Hello world',
-      },
-      {
-        id: 2,
-        title: 'Hello universe',
+    const pages = await Page.all()
+    const pagesJSON = pages.map((page) => page.serialize())
+
+    return pagesJSON
+  }
+
+  public async create(ctx: HttpContextContract) {
+    const page = new Page()
+
+    page.slug = 'home'
+    page.title = 'Home'
+
+    page.data = {
+      hero: {
+        title: 'Versterk je merk met de juiste Vibe'
       }
-    ]
+    }
+
+    await page.save()
   }
 }
