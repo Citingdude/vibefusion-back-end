023a56d0faef66eef449990891f9495ac8727877
citingdude <glenn.reumers@hotmail.com>
2022-03-05 13:40:22 +0100

    Add update function

--- app/Controllers/CasesController.ts
+++ app/Controllers/CasesController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
+import Case from 'App/Models/Case'
 
 export default class CasesController {
   public async index(ctx: HttpContextContract) {
@@ -10,17 +11,30 @@ export default class CasesController {
     return [
       {
         slug: ctx.params
+        context: ctx
       }
     ]
   }
 
   public async create(ctx: HttpContextContract) {
-    const postId = await Database
-      .table('cases')
-      .insert({
-        title: 'Adonis',
-        description: 'Let\'s learn AdonisJs'
-      })
-    .returning('id')
+    const newCase = new Case()
+
+    newCase.slug = ctx.request.body().slug
+    newCase.title = ctx.request.body().title
+    newCase.description = ctx.request.body().description
+    
+    await newCase.save()
+
+    return ctx.request.body()
+  }
+
+  public async update(ctx: HttpContextContract) {
+    const slug = 'test slug'
+
+    const oldCase = await Case.findBy('slug', slug)
+
+    oldCase.title = 'Newer title'
+
+    await oldCase.save()
   }
 }
