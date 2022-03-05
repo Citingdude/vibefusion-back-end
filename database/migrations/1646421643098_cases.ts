eb1cb42482c848146988d8cd596eacc10c9d8e7c
citingdude <glenn.reumers@hotmail.com>
2022-03-05 13:40:41 +0100

    edit case migration

--- database/migrations/1646421643098_cases.ts
+++ database/migrations/1646421643098_cases.ts
@@ -6,6 +6,7 @@ export default class Cases extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.text('slug')
       table.text('title')
       table.text('description')
       table.timestamp('created_at', { useTz: true })
