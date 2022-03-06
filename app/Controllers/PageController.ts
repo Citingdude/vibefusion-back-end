1fa45488ff5de772973f8547d6857ff4623e0c0f
citingdude <glenn.reumers@hotmail.com>
2022-03-06 10:34:30 +0100

    Create PageController

---
+++ app/Controllers/PageController.ts
@@ -0,0 +1,49 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Page from 'App/Models/Page'
+
+export default class PageController {
+  public async index(ctx: HttpContextContract) {
+    const pages = await Page.all()
+    const pagesJSON = pages.map((page) => page.serialize())
+
+    return pagesJSON
+  }
+
+  public async create(ctx: HttpContextContract) {
+    const page = new Page()
+    const body = ctx.request.body()
+
+    page.slug = body.slug
+    page.title = body.title
+    page.data = body.data
+
+    await page.save()
+  }
+
+  public async view(ctx: HttpContextContract) {
+    const slug = ctx.params.slug
+    const page = await Page.findBy('slug', slug)
+    const pageJSON = page?.serialize()
+
+    return pageJSON
+  }
+
+  public async update(ctx: HttpContextContract) {
+    const slug = ctx.params.slug
+    const body = ctx.request.body()
+    const page = await Page.findBy('slug', slug)
+    
+    page.slug = body.slug
+    page.title = body.title
+    page.data = body.data
+
+    await page?.save()
+  }
+
+  public async destroy(ctx: HttpContextContract) {
+    const slug = ctx.params.slug
+    const page = await Page.findBy('slug', slug)
+
+    await page?.delete()
+  }
+}
