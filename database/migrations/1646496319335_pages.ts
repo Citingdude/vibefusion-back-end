369cd810ac71e99d69f2ea1d31dda46603bde9bf
citingdude <glenn.reumers@hotmail.com>
2022-03-30 20:39:29 +0200

    Make slug required

--- database/migrations/1646496319335_pages.ts
+++ database/migrations/1646496319335_pages.ts
@@ -6,7 +6,7 @@ export default class Pages extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.text('slug').unique()
+      table.text('slug').unique().notNullable
       table.text('title')
       table.json('data')
       table.timestamp('created_at', { useTz: true })
