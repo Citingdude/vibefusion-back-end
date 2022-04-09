c8302733ac145ad9199c7be977ccee2896b8758e
citingdude <glenn.reumers@hotmail.com>
2022-04-09 14:45:29 +0200

    Remove mock data

--- database/seeders/Page.ts
+++ database/seeders/Page.ts
@@ -3,14 +3,6 @@ import Page from 'App/Models/Page'
 
 export default class PageSeeder extends BaseSeeder {
   public async run() {
-    const pageData = {
-      "data": {
-        "hero": {
-          "title": "Hero title"
-        }
-      }
-    }
-
     await Page.updateOrCreate({
       slug: 'home',
       title: 'Home',
