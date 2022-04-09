b06202c767a5a1d9120732ee9c1f099c1d9789a3
citingdude <glenn.reumers@hotmail.com>
2022-04-09 21:49:33 +0200

    Make slug unique + not nullable

--- database/migrations/1646421643098_cases.ts
+++ database/migrations/1646421643098_cases.ts
@@ -6,7 +6,7 @@ export default class Cases extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.text('slug')
+      table.text('slug').unique().notNullable
       table.text('title')
       table.text('description')
       table.timestamp('created_at', { useTz: true })
