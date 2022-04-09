6bfcc88145fdc5fd13a2719e3891e5cb4b466511
citingdude <glenn.reumers@hotmail.com>
2022-04-09 15:55:56 +0200

    Add services section

--- database/seeders/Page.ts
+++ database/seeders/Page.ts
@@ -7,6 +7,32 @@ export default class PageSeeder extends BaseSeeder {
       slug: 'home',
       title: 'Home',
       image: '',
+      content: {
+        "hero": {
+          "title": "<h1>Versterk je merk met de juist Vibe</h1>",
+          "button": "Onze diensten",
+          "button_alt": "Bekijk de cases"
+        },
+        "services": {
+          "title": "Onze diensten",
+          "service_1": {
+            "title": "Webdesign",
+            "button": "Meer info"
+          },
+          "service_2": {
+            "title": "Webshop",
+            "button": "Meer info"
+          },
+          "service_3": {
+            "title": "Branding",
+            "button": "Meer info"
+          },
+          "cta": {
+            "title": "Samenwerken?",
+            "button": "Contacteer ons"
+          }
+        }
+      }
     })
   }
 }
