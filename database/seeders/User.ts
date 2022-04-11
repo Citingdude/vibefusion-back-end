eb3104d0feef38fb0a7f4394e9b98421f3e9cfb3
Glenn <glenn.reumers@hotmail.com>
2022-04-11 21:05:47 +0200

    Fix TS errors for User seeder

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -3,7 +3,7 @@ import User from 'App/Models/User'
 
 export default class UserSeeder extends BaseSeeder {
   public async run () {
-    await User.updateOrCreate({
+    await User.updateOrCreate({}, {
       email: 'glenn.reumers@hotmail.com',
       password: 'password'
     })
